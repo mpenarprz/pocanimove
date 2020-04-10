f4850331e1ed95182102a6f7d9112266560214e5
MP <peczwy@gmail.com>
2020-04-10 16:46:20 +0200

    poc

--- scripts/mouse.js
+++ scripts/mouse.js
@@ -5,4 +5,11 @@ function handleMouseMove(event) {
 	var x_percent = (event.pageX * 100 / window.innerWidth).toFixed(2);
 	var y_percent = (event.pageY * 100 / window.innerHeight).toFixed(2);
 	$("#text_coord").text('x: ' + event.pageX + ' y:' + event.pageY + ' x:' + x_percent + '% y:' + y_percent + '%');
+	
+	var video = $("#video");
+	if(video !== null){
+		var duration = video.prop("duration");
+		var current_time = duration * event.pageX / window.innerWidth;
+		video.prop("currentTime", current_time);
+	}
 }
