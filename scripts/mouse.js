49c4314cf0355641c08685b935eeaba6a15cc2f1
MPenar <peczwy@gmail.com>
2020-04-19 12:55:29 +0200

    Touch

--- scripts/mouse.js
+++ scripts/mouse.js
@@ -3,13 +3,17 @@ var video = $("#video");
 
 function handleMouseMove(event) {
 	event = event || window.event;
-	var x_percent = (event.pageX * 100 / window.innerWidth).toFixed(2);
-	var y_percent = (event.pageY * 100 / window.innerHeight).toFixed(2);
-	$("#text_coord").text('x: ' + event.pageX + ' y:' + event.pageY + ' x:' + x_percent + '% y:' + y_percent + '%');
+	refresh(event.pageX, event.pageY);
+}
+
+function refresh(x,y) {
+	var x_percent = (x * 100 / window.innerWidth).toFixed(2);
+	var y_percent = (y * 100 / window.innerHeight).toFixed(2);
+	$("#text_coord").text('x: ' + x + ' y:' + y + ' x:' + x_percent + '% y:' + y_percent + '%');
 	
 	if(video !== null){
 		var duration = video.prop("duration");
-		var current_time = duration * event.pageX / window.innerWidth;
+		var current_time = duration * x / window.innerWidth;
 		video.prop("currentTime", current_time);
 	}
 }
